d1d5101fe04e2b9b5ad27c62261753ad93603d3a
lxy <756194169@qq.com>
2019-08-03 01:12:26 +0800

    add createcomponents

---
+++ src/components/Header/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="Header">
+		<h1>{{title}} </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  // 子组件使用props来接受，
+  props: {
+    title: {
+      type: String,
+      default: '喵喵电影'
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* align-items 属性定义flex子项在flex容器的当前行的侧轴（纵轴）方向上的对齐方式 */
+#Header{width: 100%;height: 50px;background: #e54847;color:#FFF; position: relative;
+border-bottom: 1px solid #e54847;}
+#Header h1{font-size: 18px;font-weight: normal;text-align: center;line-height: 50px;}
+
+</style>
---
+++ src/views/Mine/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="main">
+    <!-- 父组件传值 静态直接用属性title=""，动态用v-bind:title=""-->
+    <Header title="我的喵喵"></Header>
+    <div id="content">
+      <login></login>
+    </div>
+    <TabBar></TabBar>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+import TabBar from '@/components/TabBar'
+import Login from '@/components/Login';
+export default {
+  name:'Mine',
+  components: {
+    Header,
+    TabBar,
+    Login
+  }
+}
+</script>
+
+<style>
+
+</style>
