dbc9d5c3b5da4c4c788bbd43f7429cf5f1ae749d
lxy <756194169@qq.com>
2019-08-03 15:59:38 +0800

    add setData

--- src/components/City/index.vue
+++ src/components/City/index.vue
@@ -1,100 +1,119 @@
 <template>
 <div class="city_body">
   <div class="city_list">
-    <div class="city_hot">
-      <h2>热门城市</h2>
-      <ul class="clearfix">
-        <li>上海</li>
-        <li>北京</li>
-        <li>上海</li>
-        <li>北京</li>
-        <li>上海</li>
-        <li>北京</li>
-        <li>上海</li>
-        <li>北京</li>
-      </ul>
+        <div class="city_hot">
+            <h2>热门城市</h2>
+            <ul class="clearfix">
+                <li v-for="item in HotList" :key="item.id">{{ item.nm }}</li>
+            </ul>
+        </div>
+        <div class="city_sort" ref="city_sort">
+            <div v-for="item in CityList" :key="item.index">
+                <h2>{{ item.index }}</h2>
+                <ul>
+                    <li v-for="itemList in item.list" :key="itemList.id" >{{ itemList.nm }}</li>
+                </ul>
+            </div>	
+        </div>
     </div>
-    <div class="city_sort">
-      <div>
-        <h2>A</h2>
+    <div class="city_index">
         <ul>
-          <li>阿拉善盟</li>
-          <li>鞍山</li>
-          <li>安庆</li>
-          <li>安阳</li>
+            <li v-for="(item,idx) in CityList" :key="item.idx" 
+            @touchstart="handleToIndex(idx)">{{ item.index }}</li>
         </ul>
-      </div>
-      <div>
-        <h2>B</h2>
-        <ul>
-          <li>北京</li>
-          <li>保定</li>
-          <li>蚌埠</li>
-          <li>包头</li>
-        </ul>
-      </div>
-      <div>
-        <h2>A</h2>
-        <ul>
-          <li>阿拉善盟</li>
-          <li>鞍山</li>
-          <li>安庆</li>
-          <li>安阳</li>
-        </ul>
-      </div>
-      <div>
-        <h2>B</h2>
-        <ul>
-          <li>北京</li>
-          <li>保定</li>
-          <li>蚌埠</li>
-          <li>包头</li>
-        </ul>
-      </div>
-      <div>
-        <h2>A</h2>
-        <ul>
-          <li>阿拉善盟</li>
-          <li>鞍山</li>
-          <li>安庆</li>
-          <li>安阳</li>
-        </ul>
-      </div>
-      <div>
-        <h2>B</h2>
-        <ul>
-          <li>北京</li>
-          <li>保定</li>
-          <li>蚌埠</li>
-          <li>包头</li>
-        </ul>
-      </div>	
     </div>
-  </div>
-  <div class="city_index">
-    <ul>
-      <li>A</li>
-      <li>B</li>
-      <li>C</li>
-      <li>D</li>
-      <li>E</li>
-    </ul>
-  </div>
 </div>
 </template>
 
 <script>
 export default {
+  name:'city',
+  data(){
+    return {
+      CityList:[],
+      HotList:[]
+    }
+  },
+  mounted() {
+    this.axios('/api/cityList').then((res)=>{
+      var cities = res.data.data.cities;
+      // 数据改造格式 [{index:'B'},{nm:'北京',py: "beijing"}]
+      var{CityList,HotList} = this.ReseCitytlist(cities);
+      this.CityList = CityList;
+      this.HotList = HotList;
+    })
+    },
+    methods:{
+      ReseCitytlist(cities){
+        var CityList = [];
+        var HotList = [];
 
-}
+        // 热门城市
+        for(var i=0;i<cities.length;i++){
+          if(cities[i].isHot === 1){
+            HotList.push(cities[i])
+          }
+        }
+
+        for (var i = 0; i < cities.length; i++) {
+          var firstLetter = cities[i].py.substring(0,1).toUpperCase();
+            if(toCon(firstLetter)){//新添加
+              CityList.push( { index : firstLetter, list : [{ nm : cities[i].nm, id: cities[i].id }]})
+            }else{
+              for(var j=0;j<CityList.length;j++){
+                if(CityList[j].index === firstLetter){
+                  //r如果py字母之间相等，就累加城市
+                  CityList[j].list.push({ nm : cities[i].nm, id: cities[i].id })
+                }
+              }
+            }
+        }
+
+      //从A-M排序
+      CityList.sort((n1,n2)=>{
+        if(n1.index>n2.index){
+          return 1;
+        }else if(n1.index<n2.index){
+          return -1;
+        }else{
+          return 0
+        }
+      })
+
+        function toCon(firstLetter){
+          for (var i = 0; i < CityList.length; i++) {
+              if(CityList[i].index === firstLetter){//累加条件
+                return false;
+              }
+          }
+          return true;//新添加条件
+        }
+        console.log(CityList,HotList)
+        return {
+          CityList,
+          HotList
+        }
+      },
+      handleToIndex(idx){
+        var h2 = this.$refs.city_sort.getElementsByTagName('h2');
+        this.$refs.city_sort.parentNode.scrollTop = h2[idx].offsetTop;
+        // h2标签本身也是一个数组,与idx有一一对应关系
+      }
+    }
+  }
 </script>
 
 <style scoped>
+#content .city_body{ margin-top: 45px; display: flex; width:100%; position: fixed;
+ top: 53px; bottom: 0;}
+.city_body .city_list{ flex:1; overflow: auto; background: #FFF5F0;}
+.city_body .city_list::-webkit-scrollbar{
+    background-color:transparent;
+    width:0;
+}
 .city_body .city_hot{ margin-top: 20px;}
 .city_body .city_hot h2{ padding-left: 15px; line-height: 30px; font-size: 14px; background:#F0F0F0; font-weight: normal;}
-.city_body .city_hot ul{}
 .city_body .city_hot ul li{ float: left; background: #fff; width: 29%; height: 33px; margin-top: 15px; margin-left: 3%; padding: 0 4px; border: 1px solid #e6e6e6; border-radius: 3px; line-height: 33px; text-align: center; box-sizing: border-box;}
-.city_body .city_sort{}
 .city_body .city_sort div{ margin-top: 20px;}
 .city_body .city_sort h2{ padding-left: 15px; line-height: 30px; font-size: 14px; background:#F0F0F0; font-weight: normal;}
 .city_body .city_sort ul{ padding-left: 10px; margin-top: 10px;}
--- src/components/Search/index.vue
+++ src/components/Search/index.vue
@@ -3,34 +3,75 @@
   <div class="search_input">
     <div class="search_input_wrapper">
       <i class="iconfont icon-sousuo"></i>
-      <input type="text">
+      <input type="text" v-model="message">
     </div>					
   </div>
   <div class="search_result">
     <h3>电影/电视剧/综艺</h3>
     <ul>
-      <li>
-        <div class="img"><img src="images/movie_1.jpg"></div>
-        <div class="info">
-          <p><span>无名之辈</span><span>8.5</span></p>
-          <p>A Cool Fish</p>
-          <p>剧情,喜剧,犯罪</p>
-          <p>2018-11-16</p>
-        </div>
-      </li>
-      <li>
-        <div class="img"><img src="images/movie_1.jpg"></div>
-        <div class="info">
-          <p><span>无名之辈</span><span>8.5</span></p>
-          <p>A Cool Fish</p>
-          <p>剧情,喜剧,犯罪</p>
-          <p>2018-11-16</p>
-        </div>
+      <li v-for="item in moviesList" :key="item.id">
+          <div class="img"><img :src=" item.img | setWH('120.180')"></div>
+          <div class="info">
+              <p><span>{{ item.nm }}</span><span>{{ item.sc }}</span></p>
+              <p>{{ item.enm }}</p>
+              <p>{{ item.cat }}</p>
+              <p>{{ item.rt }}</p>
+          </div>
       </li>
     </ul>
   </div>
 </div>
 </template>
+
+<script>
+export default {
+  name:'search',
+  data() {
+    return {
+      message : '',
+      moviesList : [] 
+    }
+  },
+  methods:{
+    cancelRequest(){
+        if(typeof this.source ==='function'){
+            this.source('终止请求')
+        }
+    }
+  },
+  watch : {
+    message(newVal){//普通的watch监听
+      var that = this;
+       // 取消上一次请求
+      this.cancelRequest();
+
+      this.axios('/api/searchList?cityId=10&kw='+newVal,{
+        cancelToken: new this.axios.CancelToken(function executor(c) {
+          that.source = c;
+        })
+      }).then((res) => {
+          var msg = res.data.msg;
+          var movies = res.data.data.movies;
+          if(msg && movies){
+
+            this.moviesList = res.data.data.movies.list;
+          }
+      }).catch((err) => {
+          if (this.axios.isCancel(err)) {
+              console.log('Rquest canceled', err.message);
+               //请求如果被取消，这里是返回取消的message
+          } else {
+              //handle error
+              console.log(err);
+          }
+      })   
+      
+    },
+  }
+
+}
+</script>
+
 
 <style lang="scss">
 #content .search_body{ flex:1; overflow:auto;}
